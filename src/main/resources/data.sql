b30dcb97ec852c01954301dcb618e01e66b92914
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-01-27 13:44:37 -0800

    pc - update properties and data.sql
    we need the SQL dialects to be set for ManyToOne to work properly
    We need initialization to get data.sql to work on localhost

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users (id, email,full_name,given_name,family_name,admin,email_verified) VALUES (1000,'fake@example.org','Fake User','Fake','User',false,false);
+insert into todos (id, title, details, done, user_id) VALUES (1,'foo','bar',false,1000);
+
